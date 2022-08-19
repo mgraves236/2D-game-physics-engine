36741de853479d57fc6b94d104d8cb069c140b27
mgraves236 <m.graves236@gmail.com>
2022-08-19 23:25:52 +0200

    Circle collision info extended

--- src/game/game.js
+++ src/game/game.js
@@ -29,7 +29,7 @@ function startGame() {
     // let source2 = new bulletSource(10, loc2, vel1, velBull2);
     let massCenter = new Vector(300, 400)
     let circle = new Circle(1, massCenter,50);
-    let massCenter2 = new Vector(300, 400)
+    let massCenter2 = new Vector(370, 400)
     let circle2 = new Circle(1, massCenter2,70)
     window.requestAnimationFrame(gEngine.Core.initializeEngineCore);
 }
