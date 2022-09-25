626b960714439e58b3bc7d51f73dbf02fd418427
mgraves236 <m.graves236@gmail.com>
2022-09-25 21:22:19 +0200

    code refactor

--- src/lib/sineWave.js
+++ src/lib/sineWave.js
@@ -6,8 +6,8 @@ export class SineWave {
     constructor(x,y,s) {
         this.x = x;
         this.y = y;
-        this.length= 500000;
+        this.length = 500000; // frequency
         this.amplitude = 3;
-        this.speed = s * 0.003;
+        this.speed = s * 0.03;
     }
 }
